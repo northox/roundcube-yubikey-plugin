b24efbe7e6a479d2a92f530334be6a4a332aebe8
Danny Fullerton <northox@mantor.org>
2012-01-22 12:46:06 -0500

    initial commit

---
+++ config.inc.php
@@ -0,0 +1,10 @@
+<?php
+// enable / disable yubikey authentication
+$rcmail_config['yubikey'] = TRUE;
+
+// yubikey API key & ID needed to access the web service API. Visit https://api.yubico.com/get-api-key/
+$rcmail_config['yubikey_api_id'] = '';
+$rcmail_config['yubikey_api_key'] = '';
+
+$rcmail_config['debug_logger']['yubikey'] = 'yubikey';
+?>
