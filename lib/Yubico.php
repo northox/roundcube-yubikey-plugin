383bbb36a057940cb3f9041f49bdf2c92b3aacd4
Luis Aranguren <pizzaman@hotmail.com>
2018-04-03 13:21:26 +1000

    Fixes PHP methods cant be named as their class error
    Fixes error PHP Deprecated:  Methods with the same name as their class will not be constructors in a future version of PHP; Auth_Yubico has a deprecated constructor in /usr/share/webapps/roundcubemail/plugins/yubikey_authentication/lib/Yubico.php on line 29

--- lib/Yubico.php
+++ lib/Yubico.php
@@ -98,7 +98,7 @@ class Auth_Yubico
 	 *                                 default true)
 	 * @access public
 	 */
-	function Auth_Yubico($id, $key = '', $https = 0, $httpsverify = 1)
+	function __construct($id, $key = '', $https = 0, $httpsverify = 1)
 	{
 		$this->_id =  $id;
 		$this->_key = base64_decode($key);
